061adc74c6e496b3886cd32ba6d0080ad2a63d6c
Kartik Patel <patelkartik1910@gmail.com>
2018-07-30 02:16:37 +0530

    Tag: #2
    Fully working version 0.2 is here.
    
    ChangeLog: #9

--- app/src/main/java/com/deucate/cleartax/HomeActivity.kt
+++ app/src/main/java/com/deucate/cleartax/HomeActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.animation.AnimationUtils
+import android.view.View
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -17,9 +17,6 @@ class HomeActivity : AppCompatActivity() {
         toolbar.title = "ClearTax"
         toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"))
 
-        if (!ConnectionChecker(this).conavailable()){
-
-        }
 
         val recyclerView = findViewById<RecyclerView>(R.id.homeRecyclerView)
         val bottomRecyclerView = findViewById<RecyclerView>(R.id.bottomRecyclerView)
@@ -40,13 +37,13 @@ class HomeActivity : AppCompatActivity() {
         latestTweets.add(Tweet(result!![0].user.biggerProfileImageURL,result[0].user.name,result[0].text,result[0].createdAt))
         latestTweets.add(Tweet(result[1].user.biggerProfileImageURL,result[1].user.name,result[1].text,result[1].createdAt))
         latestTweets.add(Tweet(result[2].user.biggerProfileImageURL,result[2].user.name,result[2].text,result[2].createdAt))
-
         for(i in 0 until result.size){
             val tweet = Tweet(result[i].user.profileImageURL,result[i].user.name+" . @"+result[i].user.screenName,result[i].text)
             tweets.add(tweet)
         }
         adapter.notifyDataSetChanged()
         bottomAdapter.notifyDataSetChanged()
+        processView.visibility = View.GONE
     }
 
 
